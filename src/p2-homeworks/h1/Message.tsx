80dac48295f52ca66890e4a50003abf9df51c0cf
MSokol <mihasokol1993@gmail.com>
2022-08-17 18:14:02 +0300

    Fixed few styles. Added new component AlternativeMessage

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './Message.module.css'
+
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+const Message = (props: PropsType) => {
+    const {time, message, name, avatar} = props
+    return (
+        <div className={styles.message__comp}>
+            <div className={styles.avatar__block}>
+                <img src={avatar} alt="Profile avatar picture"/>
+            </div>
+            <div className={styles.info__block}>
+                <h2 className={styles.name}>{name}</h2>
+                <div className={styles.messageTimeWrapper}>
+                    <span className={styles.message}>{message}</span>
+                    <span className={styles.time}>{time}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Message
