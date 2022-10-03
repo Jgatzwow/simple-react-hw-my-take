64d4a255c6246c7f65ea6b21a858df77afb500dd
MSokol <mihasokol1993@gmail.com>
2022-10-03 18:31:29 +0300

    HW7 Completed

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,30 +1,50 @@
-import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
-
-type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+import React, {
+  SelectHTMLAttributes,
+  DetailedHTMLProps,
+  ChangeEvent,
+} from "react";
+import styles from "./SuperSelect.module.css";
+type DefaultSelectPropsType = DetailedHTMLProps<
+  SelectHTMLAttributes<HTMLSelectElement>,
+  HTMLSelectElement
+>;
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+  options?: any[];
+  onChangeOption?: (option: any) => void;
+};
 
-const SuperSelect: React.FC<SuperSelectPropsType> = (
-    {
-        options,
-        onChange, onChangeOption,
-        ...restProps
-    }
-) => {
-    const mappedOptions: any[] = []; // map options with key
+const SuperSelect: React.FC<SuperSelectPropsType> = ({
+  options,
+  onChange,
+  onChangeOption,
+  ...restProps
+}) => {
+  const mappedOptions: any[] = options
+    ? options.map((o, idx) => {
+        return (
+          <option className={styles.SuperSelect__option} key={idx}>
+            {o}
+          </option>
+        );
+      })
+    : []; // map options with key
 
-    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
-    }
+  const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+    onChange && onChange(e);
+    onChangeOption && onChangeOption(e.currentTarget.value);
+    // onChange, onChangeOption
+  };
 
-    return (
-        <select onChange={onChangeCallback} {...restProps}>
-            {mappedOptions}
-        </select>
-    )
-}
+  return (
+    <select
+      onChange={onChangeCallback}
+      {...restProps}
+      className={styles.SuperSelect}
+    >
+      {mappedOptions}
+    </select>
+  );
+};
 
-export default SuperSelect
+export default SuperSelect;
--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,40 +1,58 @@
-import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
-
-type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+import React, {
+  ChangeEvent,
+  InputHTMLAttributes,
+  DetailedHTMLProps,
+} from "react";
+import styles from "./SuperRadio.module.css";
+type DefaultRadioPropsType = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+  options?: any[];
+  onChangeOption?: (option: any) => void;
+};
 
-const SuperRadio: React.FC<SuperRadioPropsType> = (
-    {
-        type, name,
-        options, value,
-        onChange, onChangeOption,
-        ...restProps
-    }
-) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
-    }
+const SuperRadio: React.FC<SuperRadioPropsType> = ({
+  type,
+  name,
+  options,
+  value,
+  onChange,
+  onChangeOption,
+  ...restProps
+}) => {
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e);
+    onChangeOption && onChangeOption(e.currentTarget.name);
+    // onChange, onChangeOption
+  };
 
-
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
+  const mappedOptions: any[] = options
+    ? options.map(
+        (
+          o,
+          i // map options with key
+        ) => (
+          <label key={name + "-" + i} className={styles.Label__Super__Radio}>
             <input
-                type={'radio'}
-                // name, checked, value, onChange
+              className={styles.Super__Radio}
+              type={"radio"}
+              name={o}
+              checked={o === value}
+              value={value}
+              onChange={onChangeCallback}
+              {...restProps}
+              // name, checked, value, onChange
             />
             {o}
-        </label>
-    )) : []
+          </label>
+        )
+      )
+    : [];
 
-    return (
-        <>
-            {mappedOptions}
-        </>
-    )
-}
+  return <>{mappedOptions}</>;
+};
 
-export default SuperRadio
+export default SuperRadio;
