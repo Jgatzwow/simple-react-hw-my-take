b892db9b8ec029bb8c4b32ef1a8610530dc53747
MSokol <mihasokol1993@gmail.com>
2022-11-03 03:34:13 +0300

    hw 11 in progress

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,44 @@
-import React, {useState} from 'react'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import React, { useState } from "react";
+import SuperRange from "./common/c7-SuperRange/SuperRange";
+import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
+import "../../App.css";
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
-
-    return (
-        <div>
-            <hr/>
-            homeworks 11
-
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
-
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(100);
+  const step = 10;
+  return (
+    <div>
+      <hr />
+      homeworks 11
+      {/*should work (должно работать)*/}
+      <div className="doubleRange__wrapper">
+        <span>{value1}</span>
+        <SuperRange
+          value={value1}
+          onChangeRange={setValue1}
+          // сделать так чтоб value1 изменялось
+        />
+      </div>
+      <div className="doubleRange__wrapper">
+        <span>{value1}</span>
+        <SuperDoubleRange
+          value={[value1, value2]}
+          min={value1}
+          max={value2}
+          step={step}
+          disabled
+          // сделать так чтоб value1 и value2 изменялось
+        />
+        <span>{value2}</span>
+      </div>
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr />
+    </div>
+  );
 }
 
-export default HW11
+export default HW11;
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box, Slider } from "@mui/material";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
-}
+  onChangeRange?: (value: [number, number]) => void;
+  value?: [number, number];
+  min?: number;
+  max?: number;
+  step?: number;
+  disabled: boolean;
+  // min, max, step, disable, ...
+};
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({
+  onChangeRange,
+  value,
+  min,
+  max,
+  step,
+  disabled,
+  // min, max, step, disable, ...
+}) => {
+  const minSliderValue = value ? value[0] : 0;
+  const maxSliderValue = value ? value[1] : 100;
+  const [finalVal, setFinalVal] = useState([minSliderValue, maxSliderValue]);
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setFinalVal(newValue as number[]);
+  };
 
-    return (
-        <>
-            DoubleRange
-        </>
-    )
-}
+  function valuetext(value: number) {
+    return `${value}°C`;
+  }
 
-export default SuperDoubleRange
+  return (
+    <Box sx={{ width: 300 }}>
+      <Slider
+        getAriaLabel={() => "Temperature range"}
+        value={finalVal}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        min={min}
+        max={max}
+        step={step}
+        disabled={max === min}
+      />
+    </Box>
+  );
+};
+
+export default SuperDoubleRange;
